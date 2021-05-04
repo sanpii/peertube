982f73e798ce3401a42562723fc483e9a91396ef
Sanpi <sanpi@homecomputing.fr>
2021-05-04 11:08:07 +0200

    Adds documentation

--- src/services/accounts.rs
+++ src/services/accounts.rs
@@ -1,3 +1,6 @@
+/**
+ * <https://docs.joinpeertube.org/api-rest-reference.html#tag/Accounts>
+ */
 pub struct Accounts {
     config: crate::Config,
 }
@@ -9,22 +12,37 @@ impl Accounts {
         }
     }
 
+    /**
+     * Get an account.
+     */
     pub async fn get(&self, name: &str) -> crate::Result<crate::data::Account> {
         crate::Api::get(&self.config, &format!("/accounts/{}", name), (), None).await
     }
 
+    /**
+     * List videos of an account.
+     */
     pub async fn videos(&self, name: &str, params: &crate::param::Videos) -> crate::Result<crate::Pager<crate::data::Video>> {
         crate::Api::get(&self.config, &format!("/accounts/{}/videos", name), params, None).await
     }
 
+    /**
+     * List accounts.
+     */
     pub async fn all(&self, params: &crate::param::Accounts) -> crate::Result<crate::Pager<crate::data::Account>> {
         crate::Api::get(&self.config, "/accounts", params, None).await
     }
 
+    /**
+     * List video channels of an account.
+     */
     pub async fn video_channels(&self, name: &str, params: &crate::param::Channels) -> crate::Result<crate::Pager<crate::data::Channel>> {
         crate::Api::get(&self.config, &format!("/accounts/{}/video-channels", name), params, None).await
     }
 
+    /**
+     * List ratings of an account.
+     */
     pub async fn ratings(&self, auth: &crate::data::Token, name: &str, params: &crate::param::Ratings) -> crate::Result<crate::Pager<crate::data::Channel>> {
         crate::Api::get(&self.config, &format!("/accounts/{}/ratings", name), params, Some(auth)).await
     }
